f5c321c816c588424dfa19b78b738683c88654c4
Giselly Vieira <gisellysantosvieira@gmail.com>
2026-07-20 21:32:56 -0300

    Corrige conexao com PostgreSQL

--- app.py
+++ app.py
@@ -42,6 +42,11 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+    "pool_pre_ping": True,
+    "pool_recycle": 300,
+}
+
 db.init_app(app)
 
 
